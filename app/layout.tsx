c256fa360bf2d3c84065546ba1488208c1a75d8f
Vivek Gupta <vivekparasnath6969@gmail.com>
2026-01-15 22:41:46 +0530

    remove v0 watermark

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body className={`font-sans antialiased`}>
         {children}
         <WhatsAppButton />
-        <Analytics />
+        {/* <Analytics /> */}
       </body>
     </html>
   )
